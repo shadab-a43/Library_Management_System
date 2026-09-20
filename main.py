14aa94023693ca6baacd0f6166a6f464e166ded9
Shadab Alam <Shadab606681@gmail.com>
2026-09-20 23:12:52 +0530

    Complete Day 3 OOP Concepts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Library Managenment System
+#Library Management System
 
 #Book information
 book_title="Python Basics"
@@ -20,15 +20,15 @@ else:
 
 #List of 5 books
 books = [
-    "Pyhton Basics",
+    "Python Basics",
     "Data Structure",
-    "Database Managenment",
+    "Database Management",
     "Operating Systems",
     "Computer Networks"
 ]
 
 #Display all books
-print("\nLibrary Books:")
+print("Library Books:")
 
 for book in books:
     print(book)
@@ -42,7 +42,7 @@ def calculate_fine(days_late):
 
 fine = calculate_fine(7)                     #Calling the function
 
-print("Days Late:",5)
+print("\nDays Late:",5)
 print("Fine:",fine)
 
 #Recursive function
@@ -91,3 +91,76 @@ member1 = Member("Shadab", "shadab@example.com")
 
 print("Member name:",member1.name)
 print("Member email:",member1.email)
+
+
+## -->Day 3: Inheritance, Encapsulation & Polymorphism<--
+
+#Encapsulation
+class accountholder:                            #acountholder class
+
+    def __init__(self, name, password):         #constructor
+        self.name = name
+        self.__password = password
+
+    def change_password(self, new_password):        #method to change password
+        self.__password = new_password
+        print("Password changed successfully")
+
+account=accountholder("shadab","old123")            #creating an accountholder object
+print("\nAccount Holder:",account.name)
+
+
+account.change_password("new123")                   #changing password  
+
+#Inheritance
+class LibraryBook:
+
+    def __init__(self,title,author):                #parent class
+        self.title=title
+        self.author=author
+
+    def show_book(self):
+        print("Book Title:",self.title)
+        print("Book Author:",self.author)
+
+class Ebook(LibraryBook):                               #child class
+    pass
+
+ebook=Ebook("Python Basics","Guido van Rossum")         #creating an Ebook object
+
+ebook.show_book()                                       #using method inherited from LibraryBook
+
+#Polymorphism
+class BookType:                                     #parent class
+    def issue(self):
+        print("E-book issued")
+
+
+class EBookType(BookType):                               #child class
+
+    def issue(self):                                    #same method diffrent behavior
+        print("E-book access provided")
+
+book=BookType()                             #creating objects
+ebook=EBookType()
+
+book.issue()                              #calling the same method
+ebook.issue()
+
+#Abstraction
+from abc import ABC, abstractmethod
+
+class LibraryItem(ABC):                            #abstract class
+    
+    @abstractmethod
+    def display_info(self):                         
+        pass
+
+class LibraryBookItem(LibraryItem):                     #child class
+
+    def display_info(self):                             
+        print("This is a library book.")
+
+
+item=LibraryBookItem()                            #creating object of child class
+item.display_info()                             
